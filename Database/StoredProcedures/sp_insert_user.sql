a1282f3b9d143a2097c7c3a4bf85aae97a5e3f17
20mmeppelink <20mmeppelink@gmail.com>
2025-06-11 23:41:40 -0500

    updates sp_insert_user to handle case of updating values when the existing value is null

--- Database/StoredProcedures/sp_insert_user.sql
+++ Database/StoredProcedures/sp_insert_user.sql
@@ -17,7 +17,11 @@ BEGIN
     MERGE INTO Users AS target
     USING @Users AS source
     ON target.user_id = source.user_id
-    WHEN MATCHED AND (target.username <> source.username OR target.is_bot <> source.is_bot OR target.avatar_url <> source.avatar_url) THEN
+    WHEN MATCHED AND (
+        ISNULL(target.username, '') <> ISNULL(source.username, '') OR
+        ISNULL(target.is_bot, 0) <> ISNULL(source.is_bot, 0) OR
+        ISNULL(target.avatar_url, '') <> ISNULL(source.avatar_url, '')
+    ) THEN
         UPDATE SET 
             target.username = source.username,
             target.is_bot = source.is_bot,
